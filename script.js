44a99567237d2af5cb9a628084475f6e43b23386
Rafael de Souza <rafaelzoth11@gmail.com>
2025-04-08 16:36:26 -0300

    prototipo

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSection(id) {
+  const sections = document.querySelectorAll('.content-section');
+  sections.forEach(section => {
+    section.classList.remove('active');
+  });
+
+  document.getElementById(id).classList.add('active');
+}
